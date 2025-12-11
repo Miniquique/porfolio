514a158c6ff11b35abf3987793246a24cf394fba
David <122730856+Miniquique@users.noreply.github.com>
2025-12-11 14:52:35 +0000

    enlaces

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
     <p>d.guerrerortega@gmail.com</p>
     
     <div class="footer-social">
-        <a href="https:/www.linkedin.com/in/david-guerrero-ab7aa7297" target="_blank"><img src="img/linkedin.webp" alt="LinkedIn"></a>
+        <a href="https://www.linkedin.com/in/david-guerrero-ab7aa7297" target="_blank"><img src="img/linkedin.webp" alt="LinkedIn"></a>
         <a href="https://www.instagram.com/david__guerrero_/" target="_blank"><img src="img/Instagram.webp" alt="Instagram"></a>
     </div>
     
